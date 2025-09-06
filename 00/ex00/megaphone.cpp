8d2ebcafa0fa617a1a0410241f1ad7854255de63
osancak <osancak@student.42istanbul.com.tr>
2025-09-06 16:49:56 +0300

    megaphone

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,14 +6,14 @@
 /*   By: osancak <osancak@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 16:22:01 by osancak           #+#    #+#             */
-/*   Updated: 2025/09/06 16:40:45 by osancak          ###   ########.fr       */
+/*   Updated: 2025/09/06 16:49:16 by osancak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
-void	printUpperCase(const char *str)
+void	ft_uppercase(const char *str)
 {
 	for (int i = 0; str[i]; ++i)
 		std::cout << (char)std::toupper(str[i]);
@@ -26,8 +26,8 @@ int	main(int ac, char **av)
 	else
 	{
 		for (int i = 1; i < ac; ++i)
-			printUpperCase(av[i]);
+			ft_uppercase(av[i]);
 	}
-	std::cout << '\n';
+	std::cout << "\n";
 	return (0);
 }
